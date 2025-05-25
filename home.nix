7f13fde815f746b206745b96c318c5b129e17ead
yves <rroughpatch@proton.me>
2025-05-24 20:32:22 -0500

    meow

--- home.nix
+++ home.nix
@@ -1,22 +1,39 @@
 { config, pkgs, ... }:
 
 {
-	home = {
-		username = "lyra";
-		homeDirectory = "/home/lyra";
-		stateVersion = "25.05";
-	};
-	
-	programs = {
-		home-manager.enable = true;
+  home = {
+    username = "lyra";
+    homeDirectory = "/home/lyra";
+    stateVersion = "25.05";
+  };
 
-		git = {
-			enable = true;
-			userName = "yves";
-			userEmail = "rroughpatch@proton.me";
-			extraConfig = {
-				init.defaultBranch = "main";
-			};
-		};
-	};
+  programs = {
+    home-manager.enable = true;
+
+    git = {
+      enable = true;
+      userName = "yves";
+      userEmail = "rroughpatch@proton.me";
+      extraConfig = { init.defaultBranch = "main"; };
+    };
+
+    helix = {
+      enable = true;
+      settings = {
+        theme = "autumn_night_transparent";
+        editor.cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
+      };
+
+      languages.language = [{
+        name = "nix";
+        auto-format = true;
+        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+      }];
+
+    };
+  };
 }
