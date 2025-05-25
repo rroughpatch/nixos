74d8e5f9a6c96e973e08603ae09be183a4e1afc4
yves <rroughpatch@proton.me>
2025-05-24 21:23:33 -0500

    cleaning plus formatter added

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports = [ # Include the results of the hardware scan.
@@ -109,6 +109,7 @@
 
   environment.systemPackages = with pkgs;
     [
+      inputs.alejandra.defaultPackage."${pkgs.system}"
       #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       #  wget
     ];
--- flake.nix
+++ flake.nix
@@ -4,25 +4,31 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     hm = {
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    alejandra = {
+      url = "github:kamadorueda/alejandra/4.0.0";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, hm, ... }@inputs: {
+  outputs = { self, alejandra, hm, nixpkgs, ... }@inputs: {
+    formatter.x86_64-linux = alejandra.formatter;
     nixosConfigurations = {
-			jasmine = nixpkgs.lib.nixosSystem {
-      	system = "x86_64-linux";
-     		modules = [
-					./configuration.nix
-					hm.nixosModules.home-manager
-					{
-						home-manager.useGlobalPkgs = true;
-						home-manager.useUserPackages = true;
-						home-manager.users.lyra = ./home.nix;
-					}
-				];
-    	};
-		};
+      jasmine = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          hm.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.lyra = ./home.nix;
+          }
+        ];
+      };
+    };
   };
 }
